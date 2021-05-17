943b17c11da650d6fa6d3c9b5ace2873a03e6a59
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-17 19:11:53 +0200

    Es werden jetzt 6 Zahlen statt nur einer ausgegeben.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,31 +18,59 @@ export class HomePage {
    */
   public onLottozahlenErzeugen() {
 
+    /*
     const min = 1;
     const max = 49;
     const zufallszahl = Math.floor(Math.random() * (max - min + 1)) + min;
 
     this.lottozahlenText = zufallszahl + "";
+    */
+
+    let ergebnisStr = "";
+    const lottoZahlenArray = this.lottoZahlenErzeugen();
+    for (let i = 0; i < lottoZahlenArray.length; i++)     {
+
+        ergebnisStr += lottoZahlenArray[i] + "  ";
+    }
+
+    this.lottozahlenText = ergebnisStr;
   }
 
 
   /**
    * Methode erzeugt die anzuzeigenden Zufallszahlen (6 Zahlen von 1 bis 49).
+   *
+   * @return  Array mit sechs zufällig ausgewählten Lottozahlen, aufsteigend sortiert.
    */
-  private lottoZahlenErzeugen(): void {
+  private lottoZahlenErzeugen(): number[] {
 
+    const meinArray = [];
 
     for (let i = 1; i <= 49; i++) {
 
+        const lottozahlUndZufallszahl = { lottozahl: i, zufallszahl: Math.random() };
+        meinArray.push(lottozahlUndZufallszahl);
     }
-    
-    /*
+
     meinArray.sort(function(a,b){ // nach Attribut "zufallszahl" sortieren
         if (a.zufallszahl === b.zufallszahl) { return  0; }
         if (a.zufallszahl <   b.zufallszahl) { return -1; }
         if (a.zufallszahl >   b.zufallszahl) { return  1; }
-    });    
-    */
+    });
+
+    const ergebnisArray:number[] = [];
+
+    for (let i = 0; i < 6; i++) {
+
+        ergebnisArray.push( meinArray[i].lottozahl );
+    }
+
+    ergebnisArray.sort(function(a,b){
+        if (a < b) { return -1; }
+        if (a > b) { return  1; }
+    });
+
+    return ergebnisArray;
   }
 
 }
