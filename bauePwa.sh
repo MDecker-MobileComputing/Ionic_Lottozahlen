590a98eb4d21304d7fc76314ba9c26cc29f385c4
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-18 09:54:36 +0200

    Update bauePwa.sh

--- bauePwa.sh
+++ bauePwa.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# siehe Kommentar in bauePwa.bat
+# siehe Kommentare in bauePwa.bat
 
 ionic build --prod --output-path=docs -- --base-href "https://mdecker-mobilecomputing.github.io/Ionic_Lottozahlen/"
-
