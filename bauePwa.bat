25b4587e714165e55a19ae62ae96fb31eaa2b230
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-23 00:27:38 +0200

    bauePwa (bat+sh) angepasst

--- bauePwa.bat
+++ bauePwa.bat
@@ -5,7 +5,7 @@
 @REM Lösung für Angular: https://stackoverflow.com/a/56000397/1364368
 @REM Anpassung Lösung von Ionic ("--" vor "--base-href"): https://github.com/ionic-team/ionic-cli/issues/3600#issuecomment-436798575
 
-ionic build --prod --output-path=docs -- --base-href "https://mdecker-mobilecomputing.github.io/Ionic_Lottozahlen/"
+ionic build --prod -- --base-href "https://mdecker-mobilecomputing.github.io/Ionic_Lottozahlen/" --output-path=docs
 
 @REM Die beiden Attribute können auch in der Datei angular.json angepasst werden, aber wegen der Änderung von base-href
 @REM funktioniert dann "ionic serve" nicht mehr.
--- bauePwa.sh
+++ bauePwa.sh
@@ -2,4 +2,4 @@
 
 # siehe Kommentare in bauePwa.bat
 
-ionic build --prod --output-path=docs -- --base-href "https://mdecker-mobilecomputing.github.io/Ionic_Lottozahlen/"
+ionic build --prod -- --base-href "https://mdecker-mobilecomputing.github.io/Ionic_Lottozahlen/" --output-path=docs
