89631ffbddbd3c7604e4d93ed9eb64545cebfbcb
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-06-06 20:11:37 +0200

    Upgrade auf neue Ionic-Version; Webmanifest-Datei ist jetzt in public-Ordner, angular.json musste deshalb angepasst werden

---
+++ bauePwaFuerElDecker.bat
@@ -0,0 +1,12 @@
+
+@REM Damit die erzeugte PWA im von GitHub Pages vorgegebenen Ordner "docs" funktioniert, müssen wir beim Aufruf von "ionic build --prod"
+@REM spezielle Werte angeben.
+@REM
+@REM Lösung für Angular: https://stackoverflow.com/a/56000397/1364368
+@REM Anpassung Lösung von Ionic ("--" vor "--base-href"): https://github.com/ionic-team/ionic-cli/issues/3600#issuecomment-436798575
+
+ionic build --prod -- --base-href "https://el-decker.de/ionic-lottozahlen/" --output-path=www-eldecker
+
+
+@REM Die beiden Attribute können auch in der Datei angular.json angepasst werden, aber wegen der Änderung von base-href
+@REM funktioniert dann "ionic serve" nicht mehr.
